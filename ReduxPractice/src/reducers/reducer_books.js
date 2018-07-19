301fa047bf99e6fffe9fb77e33aaed7a303ab36b
gdcastillo <gdcastillo@ufl.edu>
2018-07-19 13:57:21 -0500

    Finished Redux App 1

--- ReduxPractice/src/reducers/reducer_books.js
+++ ReduxPractice/src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function(){
     return[
-        { title: 'Javascript: The Good Parts'},
-        { title: 'Harry Potter'},
-        { title: 'The Dark Tower'},
-        { title: 'Eloquent Ruby'}
+        { title: 'Javascript: The Good Parts', pages: 199},
+        { title: 'Harry Potter', pages: 4910},
+        { title: 'The Dark Tower', pages: 14},
+        { title: 'Eloquent Ruby', pages: 1}
     ]
 }
